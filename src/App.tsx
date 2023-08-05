71f2f8cc509240fd484ed067210e57597a6f82a3
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-08-04 22:11:08 -0300

    chore: add theme and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
-import { StatusBar } from "expo-status-bar"
 import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import { StatusBar } from "expo-status-bar"
+import { ThemeProvider } from "styled-components/native"
+
+import { Theme } from "@styles/index"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Lingo App by Pedro Rubinger!</Text>
+    <ThemeProvider theme={Theme}>
       <StatusBar style="auto" />
-    </View>
+    </ThemeProvider>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-})
