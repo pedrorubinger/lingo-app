893c8c4820beb3d7994e9ef732105b071089f0e4
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-08-06 16:06:33 -0300

    chore: add "go back" feature to ScreenHeader component

--- src/components/ScreenHeader/index.tsx
+++ src/components/ScreenHeader/index.tsx
@@ -1,19 +1,44 @@
 import React from "react"
 import { ViewProps } from "react-native"
+import { useNavigation } from "@react-navigation/native"
+import { StackNavigationProp } from "@react-navigation/stack"
+import { Entypo } from "@expo/vector-icons"
 
+import { Theme } from "@styles/index"
+import { RootStackParamList } from "@interfaces/index"
 import { Typography } from "@components/Typography"
-import { Box } from "@components/ScreenHeader/styles"
+import { Box, TitleBox, TouchableArrow } from "@components/ScreenHeader/styles"
+
+const { colors } = Theme
 
 interface Props extends ViewProps {
   title: string
+  goBack?: () => void
 }
 
-export const ScreenHeader: React.FC<Props> = ({ children, title, ...rest }) => {
+export const ScreenHeader: React.FC<Props> = ({
+  children,
+  title,
+  goBack,
+  ...rest
+}) => {
+  const navigation = useNavigation<StackNavigationProp<RootStackParamList>>()
+
+  const onPressGoBack = () => {
+    return goBack?.() || navigation.goBack()
+  }
+
   return (
     <Box {...rest}>
-      <Typography font="lg2" color="black">
-        {title}
-      </Typography>
+      <TouchableArrow onPress={onPressGoBack}>
+        <Entypo name="chevron-left" size={28} color={colors.koamaru300} />
+      </TouchableArrow>
+
+      <TitleBox>
+        <Typography font="lg2" color="black">
+          {title}
+        </Typography>
+      </TitleBox>
     </Box>
   )
 }
--- src/components/ScreenHeader/styles.ts
+++ src/components/ScreenHeader/styles.ts
@@ -7,5 +7,14 @@ const { spacing } = Theme
 export const Box = styled.View`
   width: 100%;
   margin-bottom: ${spacing.lg}px;
+
+  flex-direction: row;
+  align-items: center;
+`
+
+export const TouchableArrow = styled.TouchableOpacity``
+
+export const TitleBox = styled.View`
+  flex: 1;
   align-items: center;
 `
