cf7fbf803551b75597c3ea35b156527d4de13e6c
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-08-11 15:28:40 -0300

    style: add new colors to the palette

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,18 +1,29 @@
 export const Colors = {
   primary: "#249c70",
+
   black: "#1A1A1A",
+
   blue80: "#f3f8ff",
+
   grey80: "##f1f1f1",
   grey100: "#F5F5F5",
   grey200: "#E5E5E5",
   grey300: "#B3B3B3",
   grey500: "#808080",
   grey700: "#484848",
+
   green400: "#29B985",
+
   koamaru100: "#31385b",
   koamaru200: "#373f66",
   koamaru300: "#31385b",
+
+  orange100: "#F1C27B",
+  orange200: "#FFB07F",
+  orange400: "#F3AA60",
+
   red400: "#FE5C51",
+
   white: "#FFFFFF",
 } as const
 
