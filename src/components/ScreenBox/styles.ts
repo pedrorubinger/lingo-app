93b028cc75ef7e02133259c4841d439479f31d9f
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-08-12 23:00:30 -0300

    style: adjust padding-top on Android devices

--- src/components/ScreenBox/styles.ts
+++ src/components/ScreenBox/styles.ts
@@ -1,6 +1,8 @@
+import { StatusBar } from "react-native"
 import styled from "styled-components/native"
 
 import { Theme } from "@styles/theme"
+import { isAndroid } from "@utils/index"
 
 const { colors, spacing } = Theme
 
@@ -8,10 +10,15 @@ export const Box = styled.SafeAreaView`
   flex: 1;
 `
 
+const contentPaddingTop: number = isAndroid
+  ? (StatusBar.currentHeight as number) + spacing.md
+  : spacing.sm
+
 export const Content = styled.ScrollView.attrs(() => ({
   alwaysBounceVertical: false,
 }))`
-  padding: ${spacing.sm}px ${spacing.lg}px ${spacing.sm}px ${spacing.lg}px;
+  padding: ${contentPaddingTop}px ${spacing.lg}px ${spacing.sm}px
+    ${spacing.lg}px;
 
   background-color: ${colors.grey80};
 
