93dbb0ddaaa40ba5783264567c8d2a6561f52715
Pedro Rubinger <pedro.rubinger@gmail.com>
2024-02-24 11:33:32 -0300

    style: adjust margin of CarouselTitleBox component

--- src/modules/dashboard/screens/home/styles.ts
+++ src/modules/dashboard/screens/home/styles.ts
@@ -25,6 +25,6 @@ export const CardsTitleBox = styled.View`
 `
 
 export const CarouselTitleBox = styled.View`
-  margin-top: ${spacing.xl}px;
-  margin-bottom: ${spacing.md}px;
+  margin-top: ${spacing.xxl}px;
+  margin-bottom: ${spacing.xl}px;
 `
