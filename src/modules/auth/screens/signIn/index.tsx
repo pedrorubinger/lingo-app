14d87d7e5fe2ee7b59e4ffbd00acdb37e5f34099
Pedro Rubinger <pedro.rubinger@gmail.com>
2024-02-24 17:24:24 -0300

    chore: remove unused component

--- src/modules/auth/screens/signIn/index.tsx
+++ src/modules/auth/screens/signIn/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react"
-import { Animated, View } from "react-native"
+import { Animated } from "react-native"
 
 import { Theme } from "@styles/theme"
 import { InputSizeName } from "@styles/input"
