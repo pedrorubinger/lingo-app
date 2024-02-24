abff03f31f3f5e60bcb51a41964b18d4068e727d
Pedro Rubinger <pedro.rubinger@gmail.com>
2024-02-24 11:50:42 -0300

    refactor: remove menu from HomeHeader component

--- src/modules/dashboard/components/HomeHeader/index.tsx
+++ src/modules/dashboard/components/HomeHeader/index.tsx
@@ -1,34 +1,21 @@
 import React from "react"
-import { useNavigation } from "@react-navigation/native"
-import { StackNavigationProp } from "@react-navigation/stack"
-import { Entypo } from "@expo/vector-icons"
 
-import { Theme } from "@styles/index"
-import { RootStackParamList } from "@interfaces/index"
 import { Typography } from "@components/Typography"
 import {
   Box,
-  TouchableMenu,
   MessageBox,
 } from "@modules/dashboard/components/HomeHeader/styles"
-
-const { colors } = Theme
 
 interface Props {
   name: string
 }
 
 export const HomeHeader: React.FC<Props> = ({ name }) => {
-  const navigation = useNavigation<StackNavigationProp<RootStackParamList>>()
   const MAX_NAME_LENGTH = 11
   const label =
     name.length > MAX_NAME_LENGTH
       ? `${name.slice(0, MAX_NAME_LENGTH)}...`
       : `${name}!`
-
-  const onPressMenu = () => {
-    navigation.navigate("DashboardStack", { screen: "Menu" })
-  }
 
   return (
     <Box>
@@ -40,10 +27,6 @@ export const HomeHeader: React.FC<Props> = ({ name }) => {
           {label}
         </Typography>
       </MessageBox>
-
-      <TouchableMenu onPress={onPressMenu}>
-        <Entypo name="menu" size={32} color={colors.grey700} />
-      </TouchableMenu>
     </Box>
   )
 }
