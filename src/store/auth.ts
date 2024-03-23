0c54f057861be257749c1db34aee9d22088d18ed
Pedro Rubinger <pedro.rubinger@gmail.com>
2024-03-23 15:24:09 -0300

    feat: add the initial structure of the AuthStore

---
+++ src/store/auth.ts
@@ -0,0 +1,13 @@
+import { create } from "zustand"
+
+import { User } from "@interfaces/User"
+
+interface AuthStore {
+  user: User | null
+  setUser: (user: User | null) => void
+}
+
+export const useAuthStore = create<AuthStore>((set) => ({
+  user: null,
+  setUser: (user) => set({ user }),
+}))
